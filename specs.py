447f82585ccbd1e3f4b68c3256f9de7abdc7454c
Daniel Gruno <humbedooh@apache.org>
2019-07-23 11:32:42 +0200

    formatting

--- specs.py
+++ specs.py
@@ -8,41 +8,55 @@ MINIMUM = {
     'memory': 7200,
     'cpu': 2
 }
+def pprint(txt = None):
+    if not txt:
+        txt = '-' * 80
+    else:
+        txt = txt[:79]
+    print("# %-80s #" % txt)
 
 def main():
     mem = int((psutil.virtual_memory().total /  (1024*1024)))
     swap = int((psutil.swap_memory().total /  (1024*1024)))
     cpu = cpuinfo.get_cpu_info()
     
-    print("MEMORY: ")
-    print("- Real memory: %uMB" % mem)
-    print("- Swap available: %uMB" % swap)
-    print("CPU:")
-    print("- Cores: %u x %uMHz (%s)" % (cpu['count'], cpu['hz_advertised_raw'][0]/1000000, cpu['brand']))
-    print("DISKS:")
+    pprint()
+    print("#                        Apache Build System Spec Analyzer                         #")
+    pprint()
+    
+    pprint("MEMORY: ")
+    pprint("- Real memory: %uMB" % mem)
+    pprint("- Swap available: %uMB" % swap)
+    pprint("CPU:")
+    pprint("- Cores: %u x %uMHz (%s)" % (cpu['count'], cpu['hz_advertised_raw'][0]/1000000, cpu['brand']))
+    pprint("DISKS:")
     for disk in psutil.disk_partitions():
         if not '/snap' in disk.mountpoint:
             d = psutil.disk_usage(disk.mountpoint)
-            print("- %s: %uGB free out of %uGB total" % (disk.mountpoint, d.free/(1024*1024*1024), d.total/(1024*1024*1024)))
+            pprint("- %s: %uGB free out of %uGB total" % (disk.mountpoint, d.free/(1024*1024*1024), d.total/(1024*1024*1024)))
 
-    print("NETWORK:")
+    pprint("NETWORK:")
     ifs = psutil.net_io_counters(pernic=True)
     for net, data in psutil.net_if_addrs().items():
         if not 'AF_INET' in str(data[0].family):
             continue
-        print("- %s:" % net)
+        pprint("- %s:" % net)
         for block in data:
             if 'AF_INET' in str(block.family):
-                print("  - Protocol = %s, Address = %s, broadcast = %s" %(
+                pprint("  - Protocol = %s, Address = %s, broadcast = %s" %(
                     'IPv6' if 'AF_INET6' in str(block.family) else 'IPv4',
                     block.address,
                     block.broadcast or "??"
                 ))
     
+    pprint()
     if cpu['count'] < MINIMUM['cpu'] or mem < MINIMUM['memory']:
-        sys.stderr.write("Machine does not meet minimum requirements for Apache!\n")
+        pprint("Machine does not meet minimum requirements for Apache!\n")
+        pprint()
         sys.exit(-1)
     else:
-        print("System meets requirements for Apache.")
+        pprint('System meets Apache build standards')
+        pprint()
+
 if __name__ == '__main__':
     main()
